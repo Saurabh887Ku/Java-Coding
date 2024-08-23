7f8053176e0771b6fb46f6ab1674ae94c7112e7f
Saurabh Kumar <sk88714380@gmail.com>
2024-08-23 14:56:15 +0530

    Sixth Commit

---
+++ ARRAY/SmallestElement.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SmallestElement{
+    public static int smallestElementOfArray(int [] arr){
+        int smallest = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] < smallest){
+                smallest = arr[i];
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] Saurabh){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter array elements");
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Smallest element of array is -> "+smallestElementOfArray(arr));
+    }
+}
